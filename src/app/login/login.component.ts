0cb529addd0923724c4eb653bea6a7a795976198
Sebastiansrcb <rodri2018@gmail.com>
2023-12-17 21:04:32 -0400

    Ultimos arreglos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
-// login.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SHA256 } from 'crypto-js';
+
+interface Usuario {
+  nombreUsuario: string;
+  contrasenaEncriptada: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -11,6 +15,21 @@ import { SHA256 } from 'crypto-js';
 export class LoginComponent {
   usuario: string = '';
   contrasena: string = '';
+  mostrarMensaje: boolean = false;
+
+  usuariosRegistrados: Usuario[] = [
+    {
+      nombreUsuario: 'Sebastiansrcb',
+      contrasenaEncriptada: '70bd8714a2d856538c68cb92919d6573f4c690959e148dc133f6f043c0e41199',
+    },{
+      nombreUsuario: 'Pancho',
+      contrasenaEncriptada: '3ae643d65f437d9103f74f38ab66465aafb6e1b521a1662359d5165d447ce494',
+    },{
+      nombreUsuario: 'Marco',
+      contrasenaEncriptada: '64b782abe4dba6732a79424b55b7bf9db28f57c91bc7f92c2a122f5de78700e8',
+    },
+    // Agrega más usuarios si es necesario
+  ];
 
   constructor(private router: Router) {}
 
@@ -18,17 +37,17 @@ export class LoginComponent {
     // Encriptar la contraseña antes de compararla
     const contrasenaEncriptada = SHA256(this.contrasena).toString();
 
-    // Comparar con las credenciales correctas
-    const usuarioCorrecto = 'Sebastiansrcb';
-    const contrasenaCorrectaEncriptada = '70bd8714a2d856538c68cb92919d6573f4c690959e148dc133f6f043c0e41199';
+    // Iterar sobre la lista de usuarios registrados
+    const usuarioEncontrado = this.usuariosRegistrados.find(
+      (usuario) => usuario.nombreUsuario === this.usuario && usuario.contrasenaEncriptada === contrasenaEncriptada
+    );
 
-    if (this.usuario === usuarioCorrecto && contrasenaEncriptada === contrasenaCorrectaEncriptada) {
+    if (usuarioEncontrado) {
       // Redirigir a la página después de iniciar sesión correctamente.
       this.router.navigate(['/home']);
     } else {
       console.log('Credenciales incorrectas');
-      console.log(contrasenaCorrectaEncriptada);
-      console.log(contrasenaEncriptada);
+      this.mostrarMensaje = true;
     }
   }
 }
