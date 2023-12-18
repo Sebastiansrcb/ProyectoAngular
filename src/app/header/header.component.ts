8ef2d88ead3dd0a5de56a27a1b96a1f0b1ff12e6
Sebastiansrcb <rodri2018@gmail.com>
2023-12-18 00:03:59 -0400

    boton login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
+  
+  onlogin(){
+    this.router.navigate(['login']);
+  }
   onContribute(){
     const link=document.createElement('a');
     link.href="https://github.com/Sebastiansrcb/ProyectoAngular";
